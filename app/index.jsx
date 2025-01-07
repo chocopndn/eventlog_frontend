ae71fd7b48c281e1217a88fe2132a5e0847e9ee7
chocopndn <chocopndn@gmail.com>
2025-01-08 02:48:21 +0800

    UPDATE: Initial screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+
+import images from "../constants/images";
+import { StatusBar } from "expo-status-bar";
+
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View>
-      <Text className="text-3xl font-SquadaOne">Squada One</Text>
-      <Text className="text-3xl font-Arial">Arial</Text>
+    <View className="items-center justify-center h-[100%]">
+      <Text className="text-[80px] font-SquadaOne color-primary">EVENTLOG</Text>
+      <View className="mt-5 mb-7">
+        <Image source={images.logo} className="w-52 h-52" />
+      </View>
+      <Text className="font-SquadaOne color-primary text-[29px]">
+        Every CIT Event's Companion
+      </Text>
+
+      <View className="mt-5">
+        <CustomButton type="primary" title="LOG IN" />
+        <CustomButton type="disabled" title="REGISTER" />
+      </View>
+
+      <StatusBar style="auto" />
     </View>
   );
 }
