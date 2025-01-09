3b64e1403e7603477e2c17d93fd6d358d9f4284a
chocopndn <chocopndn@gmail.com>
2025-01-09 15:55:20 +0800

    ADD: Icons directory

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,15 @@
 import logo from "../assets/images/logo.png";
 
+// icons
+import lock from "../assets/icons/lock.png";
+import idBadge from "../assets/icons/id-badge.png";
+import view from "../assets/icons/view.png";
+import hide from "../assets/icons/hide.png";
+
 export default {
   logo,
+  lock,
+  idBadge,
+  view,
+  hide,
 };
