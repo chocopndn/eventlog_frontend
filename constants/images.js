2a653951ed961fb5458c483de9801edb92068441
chocopndn <chocopndn@gmail.com>
2025-01-10 21:43:15 +0800

    ADD: Icons

--- constants/images.js
+++ constants/images.js
@@ -5,6 +5,10 @@ import lock from "../assets/icons/lock.png";
 import idBadge from "../assets/icons/id-badge.png";
 import view from "../assets/icons/view.png";
 import hide from "../assets/icons/hide.png";
+import home from "../assets/icons/home.png";
+import scanner from "../assets/icons/scanner.png";
+import calendar from "../assets/icons/calendar.png";
+import user from "../assets/icons/user.png";
 
 export default {
   logo,
@@ -12,4 +16,8 @@ export default {
   idBadge,
   view,
   hide,
+  home,
+  scanner,
+  calendar,
+  user,
 };
