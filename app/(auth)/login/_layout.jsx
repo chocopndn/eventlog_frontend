b2f7762f05d45d686a7b4d6fc7c5fb2dc86ab7b4
chocopndn <chocopndn@gmail.com>
2025-03-22 01:16:41 +0800

    Added forgotpassword layout

--- app/(auth)/login/_layout.jsx
+++ app/(auth)/login/_layout.jsx
@@ -1,9 +1,24 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+
+import theme from "../../../constants/theme";
 
 const LoginLayout = () => {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="ForgotPassword"
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+          headerTintColor: theme.colors.secondary,
+        }}
+      />
     </Stack>
   );
 };
