d7575f22bf3aa8e061516dc52e57dee72eccf9c6
chocopndn <chocopndn@gmail.com>
2025-03-23 21:43:43 +0800

    Added store user

--- database/queries.js
+++ database/queries.js
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 import initDB from "./database";
 
 export const storeDepartments = async (departments) => {
-  if (Platform.OS === "android" || Platform === "ios") {
+  if (Platform.OS === "android" || Platform.OS === "ios") {
     try {
       const dbInstance = await initDB();
       if (!dbInstance) {
@@ -21,7 +21,32 @@ export const storeDepartments = async (departments) => {
     } catch (error) {
       console.error("Error storing departments:", error);
     }
-  } else {
-    console.log("WEB");
+  }
+};
+
+export const storeUser = async (user) => {
+  if (Platform.OS !== "web") {
+    try {
+      const dbInstance = await initDB();
+      if (!dbInstance) {
+        return;
+      }
+
+      const insertQuery =
+        "INSERT OR IGNORE INTO users (id_number, role_id, block_id, first_name, middle_name, last_name, suffix, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+      await dbInstance.runAsync(insertQuery, [
+        user.id_number,
+        user.role_id,
+        user.block_id,
+        user.first_name,
+        user.middle_name,
+        user.last_name,
+        user.suffix,
+        user.email,
+      ]);
+    } catch (error) {
+      console.error("Error storing user", error);
+    }
   }
 };
