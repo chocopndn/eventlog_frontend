e869cae9819b42afacd3808393c1510f0f63eddf
chocopndn <chocopndn@gmail.com>
2025-03-25 12:43:18 +0800

    Added welcome screen

--- app/(drawer)/(tabs)/home/_layout.jsx
+++ app/(drawer)/(tabs)/home/_layout.jsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
 
+import theme from "../../../../constants/theme";
+
 const HomeLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="Welcome" options={{ headerShown: false }} />
     </Stack>
   );
 };
