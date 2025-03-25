bf29069a70c64b160134c1486cea89a080ce32c2
chocopndn <chocopndn@gmail.com>
2025-03-25 12:55:53 +0800

    Fixed welcome layout

--- app/(drawer)/(tabs)/home/Welcome.jsx
+++ app/(drawer)/(tabs)/home/Welcome.jsx
@@ -12,50 +12,46 @@ import images from "../../../../constants/images";
 const Welcome = () => {
   return (
     <SafeAreaView style={globalStyles.secondaryContainer}>
-      <View style={styles.container}>
-        <View style={styles.welcomContainer}>
-          <Text style={styles.welcomeText}>WELCOME EVENTLOG USERS!</Text>
-        </View>
-        <View style={styles.info}>
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>
-              The College of Information Technology proudly introduce EVENTLOG,
-              the new mobile application - event attendance monitoring system,
-              designed to easily monitor and gather the attendance of each
-              student in a techy-way!
-            </Text>
-            <Text style={styles.infoText}>
-              EVENTLOG streamlines the attendance process, making it faster,
-              more efficient, and friendly. With the power of QR codes, you can
-              now easily log in/out your attendance during events with just a
-              quick scan.
-            </Text>
-            <Text style={styles.infoText}>
-              Join us in embracing this innovative solution that enhances your
-              event experience and simplifies attendance logging. Welcome and
-              happy logging!
-            </Text>
-            <Text style={styles.infoText}>
-              For more information/inquiries feel free to contact us:
-            </Text>
+      <View style={styles.info}>
+        <View style={styles.infoContainer}>
+          <View style={styles.welcomeContainer}>
+            <Text style={styles.welcomeText}>WELCOME EVENTLOG USERS!</Text>
+          </View>
+          <Text style={styles.infoText}>
+            The College of Information Technology proudly introduce EVENTLOG,
+            the new mobile application - event attendance monitoring system,
+            designed to easily monitor and gather the attendance of each student
+            in a techy-way!
+          </Text>
+          <Text style={styles.infoText}>
+            EVENTLOG streamlines the attendance process, making it faster, more
+            efficient, and friendly. With the power of QR codes, you can now
+            easily log in/out your attendance during events with just a quick
+            scan.
+          </Text>
+          <Text style={styles.infoText}>
+            Join us in embracing this innovative solution that enhances your
+            event experience and simplifies attendance logging. Welcome and
+            happy logging!
+          </Text>
+          <Text style={styles.infoText}>
+            For more information/inquiries feel free to contact us:
+          </Text>
 
-            <View style={styles.contactsContainer}>
-              <View style={styles.emailContainer}>
-                <Image source={images.email} style={styles.icon} />
-                <Text style={styles.infoText}>
-                  cit_eventlogsupport@gmail.com
-                </Text>
-              </View>
-              <View style={styles.facebookContainer}>
-                <Image source={images.facebook} style={styles.icon} />
-                <Text style={styles.infoText}>CITofficial.UCV</Text>
-              </View>
-              <View style={styles.locationContainer}>
-                <Image source={images.location} style={styles.icon} />
-                <Text style={styles.infoText}>
-                  CIT Office - VHNPB Building 4th Floor
-                </Text>
-              </View>
+          <View style={styles.contactsContainer}>
+            <View style={styles.emailContainer}>
+              <Image source={images.email} style={styles.icon} />
+              <Text style={styles.infoText}>cit_eventlogsupport@gmail.com</Text>
+            </View>
+            <View style={styles.facebookContainer}>
+              <Image source={images.facebook} style={styles.icon} />
+              <Text style={styles.infoText}>CITofficial.UCV</Text>
+            </View>
+            <View style={styles.locationContainer}>
+              <Image source={images.location} style={styles.icon} />
+              <Text style={styles.infoText}>
+                CIT Office - VHNPB Building 4th Floor
+              </Text>
             </View>
           </View>
         </View>
@@ -73,7 +69,7 @@ const styles = StyleSheet.create({
     paddingRight: theme.spacing.medium,
     paddingBottom: theme.spacing.medium,
   },
-  welcomContainer: {
+  welcomeContainer: {
     backgroundColor: theme.colors.primary,
     height: 50,
     alignItems: "center",
@@ -87,8 +83,7 @@ const styles = StyleSheet.create({
   info: {
     flex: 1,
     backgroundColor: theme.colors.primary,
-    borderTopColor: theme.colors.secondary,
-    borderTopWidth: 2,
+
     marginBottom: 20,
   },
   infoContainer: {
