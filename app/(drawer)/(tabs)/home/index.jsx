82817007fa2727b208b8d96110c276f26864cf5e
chocopndn <chocopndn@gmail.com>
2025-03-25 16:01:59 +0800

    Fixed layout

--- app/(drawer)/(tabs)/home/index.jsx
+++ app/(drawer)/(tabs)/home/index.jsx
@@ -1,13 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
 
 import globalStyles from "../../../../constants/globalStyles";
+import theme from "../../../../constants/theme";
+
+const screenWidth = Dimensions.get("window").width;
 
 const Home = () => {
   return (
     <View style={globalStyles.secondaryContainer}>
-      <Text>Home</Text>
+      <Text style={styles.textHeader}>EVENTLOG</Text>
+      <Text style={styles.title}>LIST OF EVENTS</Text>
+      <View style={styles.line}></View>
+      <TouchableOpacity onPress={() => router.push("/home/Welcome")}>
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcomeText}>WELCOME EVENTLOG USERS!</Text>
+        </View>
+      </TouchableOpacity>
+      <ScrollView></ScrollView>
+
       <StatusBar style="light" />
     </View>
   );
@@ -15,4 +35,37 @@ const Home = () => {
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: theme.fontSizes.display,
+    fontFamily: "SquadaOne",
+    color: theme.colors.primary,
+    paddingBottom: theme.spacing.large,
+    paddingTop: theme.spacing.large,
+  },
+  title: {
+    fontSize: theme.fontSizes.huge,
+    fontFamily: "SquadaOne",
+    color: theme.colors.primary,
+  },
+  line: {
+    borderColor: theme.colors.primary,
+    borderWidth: 1,
+    width: screenWidth * 0.8,
+    height: 2,
+  },
+  welcomeContainer: {
+    width: screenWidth * 0.8,
+    height: 50,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    marginTop: theme.spacing.large,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    fontFamily: "SquadaOne",
+    fontSize: theme.fontSizes.large,
+    color: theme.colors.primary,
+  },
+});
