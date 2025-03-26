118ffdee23bc98092159b2f83c96fd3532749bb6
chocopndn <chocopndn@gmail.com>
2025-03-26 11:58:50 +0800

    Added addevent and editevent screen

--- app/(drawer)/(tabs)/account/_layout.jsx
+++ app/(drawer)/(tabs)/account/_layout.jsx
@@ -4,6 +4,8 @@ const AccountLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="AddEvent" options={{ headerShown: false }} />
+      <Stack.Screen name="EditEvent" options={{ headerShown: false }} />
     </Stack>
   );
 };
