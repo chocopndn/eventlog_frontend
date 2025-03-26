0eb2a84e3cbd1aadef991323c0ba74f2e5ab293f
chocopndn <chocopndn@gmail.com>
2025-03-26 21:46:52 +0800

    Fixed date formatting

--- components/DateTimePicker.jsx
+++ components/DateTimePicker.jsx
@@ -1,111 +1,61 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-
 import theme from "../constants/theme";
 
-const DatePickerComponent = ({
-  type = "date",
-  label,
-  title,
-  onDateChange,
-  selectedDates,
-  mode,
-}) => {
+const DatePickerComponent = ({ type = "date", label, title, onDateChange }) => {
   const [date, setDate] = useState(null);
   const [showPicker, setShowPicker] = useState(false);
 
-  const onChange = (event, selectedDate) => {
+  const handleDateChange = (event, selectedDate) => {
     setShowPicker(false);
     if (selectedDate) {
       setDate(selectedDate);
-      if (onDateChange) {
-        if (mode === "multiple") {
-          if (selectedDates) {
-            const newDate = selectedDate;
-            const existingDateIndex = selectedDates.findIndex(
-              (d) => d.toDateString() === newDate.toDateString()
-            );
-            if (existingDateIndex > -1) {
-              const updatedDates = [...selectedDates];
-              updatedDates.splice(existingDateIndex, 1);
-              onDateChange(updatedDates);
-            } else {
-              onDateChange([...selectedDates, newDate]);
-            }
-          } else {
-            onDateChange([newDate]);
-          }
-        } else {
-          onDateChange(selectedDate);
-        }
-      }
+      onDateChange?.(
+        type === "date"
+          ? selectedDate.toISOString().split("T")[0]
+          : selectedDate.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+      );
     }
   };
 
   const handleLongPress = () => {
     setDate(null);
-    if (onDateChange) {
-      if (mode === "multiple") {
-        onDateChange([]);
-      } else {
-        onDateChange(null);
-      }
-    }
+    onDateChange?.(null);
   };
 
   const formattedDate = date
     ? type === "date"
-      ? date.toDateString()
+      ? date.toISOString().split("T")[0]
       : date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
     : type === "date"
     ? "Select a date"
     : "Select a time";
 
-  const displayValue =
-    mode === "multiple" && selectedDates && selectedDates.length > 0
-      ? selectedDates.map((d) => d.toDateString()).join(", ")
-      : formattedDate;
-
   return (
     <View>
-      {title ? <Text style={styles.titleText}>{title}</Text> : null}
+      {title && <Text style={styles.titleText}>{title}</Text>}
       <View style={styles.container}>
         <TouchableOpacity
           style={styles.dateButton}
           onPress={() => setShowPicker(true)}
           onLongPress={handleLongPress}
         >
-          {label ? <Text style={styles.label}>{label}</Text> : null}
-          <Text
-            style={[
-              styles.dateDisplay,
-              (!date && mode !== "multiple" && styles.placeholderText) ||
-                (mode === "multiple" &&
-                  (!selectedDates || selectedDates.length === 0) &&
-                  styles.placeholderText),
-            ]}
-          >
-            {displayValue === ""
-              ? type === "date"
-                ? "Select date(s)"
-                : "Select a time"
-              : displayValue}
+          {label && <Text style={styles.label}>{label}</Text>}
+          <Text style={[styles.dateDisplay, !date && styles.placeholderText]}>
+            {formattedDate}
           </Text>
         </TouchableOpacity>
 
         {showPicker && (
           <DateTimePicker
-            value={
-              date ||
-              (mode === "multiple" && selectedDates && selectedDates.length > 0
-                ? selectedDates[0]
-                : new Date()) ||
-              new Date()
-            }
+            value={date || new Date()}
             mode={type}
             display="default"
-            onChange={onChange}
+            onChange={handleDateChange}
             is24Hour={true}
           />
         )}
