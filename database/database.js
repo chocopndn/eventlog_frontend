1e48735875b8514305b5b2f972c1e20ce04950a7
chocopndn <chocopndn@gmail.com>
2025-03-26 11:11:17 +0800

    Denormalized tables

--- database/database.js
+++ database/database.js
@@ -16,93 +16,25 @@ const initDB = async () => {
         await db.execAsync(`PRAGMA journal_mode = WAL;`);
 
         await db.execAsync(`
-          CREATE TABLE IF NOT EXISTS roles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
-          );
-          CREATE TABLE IF NOT EXISTS departments (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE,
-            code TEXT NOT NULL UNIQUE
-          );
-          CREATE TABLE IF NOT EXISTS year_levels (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
-          );
-          CREATE TABLE IF NOT EXISTS courses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE,
-            department_id INTEGER NOT NULL
-          );
-          CREATE TABLE IF NOT EXISTS blocks (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL UNIQUE,
-            course_id INTEGER NOT NULL,
-            year_level_id INTEGER NOT NULL,
-            department_id INTEGER
-          );
           CREATE TABLE IF NOT EXISTS users (
             id_number TEXT PRIMARY KEY,
-            role_id INTEGER NOT NULL,
-            block_id INTEGER NULL,
-            department_id INTEGER NOT NULL,
             first_name TEXT NOT NULL,
             middle_name TEXT,
             last_name TEXT NOT NULL,
             suffix TEXT,
-            email TEXT UNIQUE
+            email TEXT UNIQUE,
+            role_id INTEGER NOT NULL,
+            role_name INTEGER NOT NULL,
+            block_id INTEGER NULL,
+            block_name INTEGER NULL,
+            department_id INTEGER NULL,
+            department_name INTEGER NULL,
+            department_code INTEGER NULL,
+            course_id INTEGER NULL,
+            course_name TEXT NULL,
+            year_level_id INTEGER NULL,
+            year_level_name TEXT NULL
           );
-          CREATE TABLE IF NOT EXISTS event_names (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE
-          );
-          CREATE TABLE IF NOT EXISTS events (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            event_name_id INTEGER NOT NULL,
-            venue TEXT NOT NULL,
-            description TEXT NOT NULL,
-            scan_personnel TEXT NOT NULL,
-            status TEXT NOT NULL,
-            created_by TEXT NOT NULL,
-            approved_by TEXT
-          );
-          CREATE TABLE IF NOT EXISTS event_dates (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            event_id INTEGER NOT NULL,
-            event_date DATE NOT NULL,
-            am_in TIME,
-            am_out TIME,
-            pm_in TIME,
-            pm_out TIME,
-            duration INTEGER
-          );
-          CREATE TABLE IF NOT EXISTS event_blocks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            event_id INTEGER NOT NULL,
-            block_id INTEGER
-          );
-          CREATE TABLE IF NOT EXISTS attendance (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            event_date_id INTEGER NOT NULL,
-            student_id_number TEXT NOT NULL,
-            am_in TIME,
-            am_out TIME,
-            pm_in TIME,
-            pm_out TIME
-          );
-          INSERT OR IGNORE INTO roles (name)
-          VALUES
-            ('Student'),
-            ('Officer'),
-            ('Admin'),
-            ('Super Admin');
-
-          INSERT OR IGNORE INTO year_levels (name)
-          VALUES
-            ('1st Year'),
-            ('2nd Year'),
-            ('3rd Year'),
-            ('4th Year');
         `);
 
         return db;
