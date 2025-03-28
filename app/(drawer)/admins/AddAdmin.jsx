aaff8628766a3e7552b88bf5d1b55c504c2ffb37
chocopndn <chocopndn@gmail.com>
2025-03-29 02:45:17 +0800

    Added add admin screen

---
+++ app/(drawer)/admins/AddAdmin.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddAdmin = () => {
+  return (
+    <View>
+      <Text>AddAdmin</Text>
+    </View>
+  );
+};
+
+export default AddAdmin;
+
+const styles = StyleSheet.create({});
--- app/(drawer)/admins/_layout.jsx
+++ app/(drawer)/admins/_layout.jsx
@@ -1,9 +1,19 @@
 import { Stack } from "expo-router";
 
+import theme from "../../../constants/theme";
+
 const AdminsLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="AddAdmin"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
     </Stack>
   );
 };
