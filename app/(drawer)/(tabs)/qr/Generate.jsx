5af80eb128fb80daa5c920d426c8e6bb6413b0f3
chocopndn <chocopndn@gmail.com>
2025-03-28 15:19:24 +0800

    Added screens

---
+++ app/(drawer)/(tabs)/qr/Generate.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Generate = () => {
+  return (
+    <View>
+      <Text>Generate</Text>
+    </View>
+  );
+};
+
+export default Generate;
+
+const styles = StyleSheet.create({});
--- app/(drawer)/(tabs)/qr/_layout.jsx
+++ app/(drawer)/(tabs)/qr/_layout.jsx
@@ -4,6 +4,8 @@ const QRLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="Generate" />
+      <Stack.Screen name="Scan" />
     </Stack>
   );
 };
