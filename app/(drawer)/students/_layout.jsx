de097dd8cf302bea94e1b519582d1284e5e4159a
chocopndn <chocopndn@gmail.com>
2025-03-31 18:08:51 +0800

    Added screens

--- app/(drawer)/students/_layout.jsx
+++ app/(drawer)/students/_layout.jsx
@@ -1,11 +1,37 @@
 import { Stack } from "expo-router";
 
-const StudentsLayout = () => {
+import theme from "../../../constants/theme";
+
+const DepartmentsLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="AddStudent"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EditStudent"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="StudentDetails"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
     </Stack>
   );
 };
 
-export default StudentsLayout;
+export default DepartmentsLayout;
