2c32160547f829f34d2449aba43d5ee3a6f237d4
chocopndn <chocopndn@gmail.com>
2025-03-31 10:13:47 +0800

    Fixed layout

--- app/(drawer)/departments/index.jsx
+++ app/(drawer)/departments/index.jsx
@@ -1,13 +1,238 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  RefreshControl,
+} from "react-native";
 import TabsComponent from "../../../components/TabsComponent";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { fetchDepartments, deleteDepartment } from "../../../services/api";
+import { router, useFocusEffect } from "expo-router";
+
+import images from "../../../constants/images";
+import SearchBar from "../../../components/CustomSearch";
+import CustomModal from "../../../components/CustomModal";
+import CustomButton from "../../../components/CustomButton";
 
 import globalStyles from "../../../constants/globalStyles";
+import theme from "../../../constants/theme";
 
 export default function DepartmentsScreen() {
+  const [departments, setDepartments] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
+  const [departmentToDelete, setDepartmentToDelete] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadDepartments = async () => {
+    try {
+      const fetchedDepartments = await fetchDepartments();
+      setDepartments(
+        Array.isArray(fetchedDepartments) ? fetchedDepartments : []
+      );
+    } catch (err) {}
+  };
+
+  const refreshData = async () => {
+    setRefreshing(true);
+    try {
+      await loadDepartments();
+    } catch (error) {
+    } finally {
+      setRefreshing(false);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      loadDepartments();
+    }, [])
+  );
+
+  const filteredDepartments = Array.isArray(departments)
+    ? departments.filter((dept) => {
+        const label = dept.label?.toLowerCase() || "";
+        const value = dept.value?.toString().toLowerCase() || "";
+        const query = searchQuery.toLowerCase();
+        return label.includes(query) || value.includes(query);
+      })
+    : [];
+
+  const handleDeletePress = (department) => {
+    setDepartmentToDelete(department);
+    setIsDeleteModalVisible(true);
+  };
+
+  const handleDeleteModalClose = () => {
+    setIsDeleteModalVisible(false);
+    setDepartmentToDelete(null);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (!departmentToDelete) return;
+
+    try {
+      await deleteDepartment(departmentToDelete.value);
+      setDepartments((prevDepartments) =>
+        prevDepartments.filter(
+          (dept) => dept.value !== departmentToDelete.value
+        )
+      );
+      handleDeleteModalClose();
+      setIsSuccessModalVisible(true);
+    } catch (error) {}
+  };
+
   return (
-    <View style={globalStyles.secondaryContainer}>
-      <Text>Departments</Text>
+    <SafeAreaView style={[globalStyles.secondaryContainer, { paddingTop: 0 }]}>
+      <Text style={styles.headerText}>DEPARTMENTS</Text>
+      <View style={{ paddingHorizontal: theme.spacing.medium, width: "100%" }}>
+        <SearchBar
+          placeholder="Search departments..."
+          onSearch={(query) => setSearchQuery(query)}
+        />
+      </View>
+      <ScrollView
+        style={{ flex: 1, width: "100%", marginBottom: 70 }}
+        contentContainerStyle={[styles.scrollview, { paddingBottom: 80 }]}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={refreshData} />
+        }
+      >
+        {filteredDepartments.length > 0 ? (
+          filteredDepartments.map((department) => (
+            <TouchableOpacity
+              key={department.value}
+              style={styles.departmentContainer}
+              onPress={() =>
+                router.push(
+                  `/departments/DepartmentDetails?id=${department.value}`
+                )
+              }
+            >
+              <View>
+                <Text style={styles.name}>{department.department_name}</Text>
+                <Text style={styles.departmentCode}>{department.label}</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    if (department.value) {
+                      router.push(
+                        `/departments/EditDepartment?id=${department.value}`
+                      );
+                    }
+                  }}
+                >
+                  <Image source={images.edit} style={styles.icon} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleDeletePress(department)}>
+                  <Image source={images.trash} style={styles.icon} />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          ))
+        ) : (
+          <Text style={styles.noResults}>No departments found</Text>
+        )}
+      </ScrollView>
+
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          title="ADD DEPARTMENT"
+          onPress={() => router.push("/departments/AddDepartment")}
+        />
+      </View>
+
+      <CustomModal
+        visible={isDeleteModalVisible}
+        title="Confirm Deletion"
+        message={`Are you sure you want to delete ${departmentToDelete?.label}?`}
+        type="warning"
+        onClose={handleDeleteModalClose}
+        onConfirm={handleConfirmDelete}
+        cancelTitle="Cancel"
+        confirmTitle="Delete"
+      />
+
+      <CustomModal
+        visible={isSuccessModalVisible}
+        title="Success"
+        message="Department deleted successfully!"
+        type="success"
+        onClose={() => setIsSuccessModalVisible(false)}
+        cancelTitle="CLOSE"
+      />
+
       <TabsComponent />
-    </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    color: theme.colors.primary,
+    fontFamily: theme.fontFamily.SquadaOne,
+    fontSize: 60,
+    textAlign: "center",
+    marginBottom: theme.spacing.medium,
+  },
+  departmentContainer: {
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    flexDirection: "row",
+    height: 50,
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: theme.spacing.small,
+    marginBottom: theme.spacing.small,
+  },
+  scrollview: {
+    padding: theme.spacing.medium,
+    flexGrow: 1,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    tintColor: theme.colors.primary,
+    marginLeft: theme.spacing.small,
+  },
+  iconContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  name: {
+    fontFamily: theme.fontFamily.SquadaOne,
+    color: theme.colors.primary,
+    fontSize: theme.fontSizes.large,
+  },
+  departmentCode: {
+    fontFamily: theme.fontFamily.SquadaOne,
+    color: theme.colors.primary,
+    fontSize: theme.fontSizes.small,
+  },
+  noResults: {
+    textAlign: "center",
+    fontFamily: theme.fontFamily.SquadaOne,
+    color: theme.colors.primary,
+    fontSize: theme.fontSizes.medium,
+    marginTop: theme.spacing.medium,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: theme.spacing.medium,
+    alignSelf: "center",
+    width: "80%",
+    padding: theme.spacing.medium,
+    marginBottom: 80,
+  },
+});
