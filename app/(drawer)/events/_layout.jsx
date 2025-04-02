26532d0c6d7e7841793760409b97d849903257f9
chocopndn <chocopndn@gmail.com>
2025-04-02 20:01:55 +0800

    Added screns

--- app/(drawer)/events/_layout.jsx
+++ app/(drawer)/events/_layout.jsx
@@ -1,9 +1,67 @@
 import { Stack } from "expo-router";
 
+import theme from "../../../constants/theme";
+
 const EventsLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="PendingEvents"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="AddEvent"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EditEvent"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EventDetails"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EventsList"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EditEventNames"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
+      <Stack.Screen
+        name="EventNames"
+        options={{
+          headerTitle: "",
+          headerTintColor: theme.colors.secondary,
+          headerStyle: { backgroundColor: theme.colors.primary },
+        }}
+      />
     </Stack>
   );
 };
