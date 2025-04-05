700001f5ecbdc9535d92104db21a8211ad0109d8
chocopndn <chocopndn@gmail.com>
2025-04-06 02:55:06 +0800

    Fixed layout

--- app/(drawer)/eventManagement/_layout.jsx
+++ app/(drawer)/eventManagement/_layout.jsx
@@ -2,20 +2,11 @@ import { Stack } from "expo-router";
 
 import theme from "../../../constants/theme";
 
-const AcademicManagementLayout = () => {
+const EventManagementLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="records"
-        options={{
-          headerTintColor: theme.colors.secondary,
-          headerStyle: {
-            backgroundColor: theme.colors.primary,
-          },
-          title: "Records",
-        }}
-      />
+
       <Stack.Screen
         name="events/index"
         options={{
@@ -84,6 +75,17 @@ const AcademicManagementLayout = () => {
         }}
       />
       <Stack.Screen
+        name="eventnames/AddEventName"
+        options={{
+          headerShadowVisible: false,
+          headerTintColor: theme.colors.primary,
+          headerStyle: {
+            backgroundColor: theme.colors.secondary,
+          },
+          title: "",
+        }}
+      />
+      <Stack.Screen
         name="eventnames/EventNameDetails"
         options={{
           headerShadowVisible: false,
@@ -120,4 +122,4 @@ const AcademicManagementLayout = () => {
   );
 };
 
-export default AcademicManagementLayout;
+export default EventManagementLayout;
