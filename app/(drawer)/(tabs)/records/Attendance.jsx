1a95e31c37c4e40034638eb03c292fc4e2e5ed60
chocopndn <chocopndn@gmail.com>
2025-04-11 15:42:41 +0800

    Fixed layout

--- app/(drawer)/(tabs)/records/Attendance.jsx
+++ app/(drawer)/(tabs)/records/Attendance.jsx
@@ -1,14 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import theme from "../../../../constants/theme";
+import globalStyles from "../../../../constants/globalStyles";
+
 const Attendance = () => {
   return (
-    <View>
-      <Text>Attendance</Text>
+    <View style={globalStyles.secondaryContainer}>
+      <View style={styles.attendanceWrapper}></View>
     </View>
   );
 };
 
 export default Attendance;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  attendanceWrapper: {
+    flex: 1,
+    width: "100%",
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+  },
+});
