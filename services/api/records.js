381aaf6889db543015d636a1403fb8c7985b0740
chocopndn <chocopndn@gmail.com>
2025-04-14 19:17:28 +0800

    Added fetch all ongoing and past events

--- services/api/records.js
+++ services/api/records.js
@@ -62,3 +62,55 @@ export const fetchUserPastEvents = async (
     throw error;
   }
 };
+
+export const fetchAllPastEvents = async (page = 1, limit = 10, search = "") => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/api/attendance/admin/past/events`,
+      {
+        id_number: idNumber,
+        page,
+        limit,
+        search,
+      }
+    );
+
+    if (response.data.success) {
+      return response.data;
+    }
+
+    throw new Error(
+      response.data.message || "Failed to fetch all past events."
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchAllOngoingEvents = async (
+  page = 1,
+  limit = 10,
+  search = ""
+) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/api/attendance/admin/past/events`,
+      {
+        id_number: idNumber,
+        page,
+        limit,
+        search,
+      }
+    );
+
+    if (response.data.success) {
+      return response.data;
+    }
+
+    throw new Error(
+      response.data.message || "Failed to fetch all ongoing events."
+    );
+  } catch (error) {
+    throw error;
+  }
+};
