12e720d6b7a14925f3b4950f0da1a3f0e337089f
chocopndn <chocopndn@gmail.com>
2025-05-22 14:50:50 +0800

    Fix layout

--- app/(drawer)/(tabs)/center/index.jsx
+++ app/(drawer)/(tabs)/center/index.jsx
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { getRoleID } from "../../../../database/queries";
 
 import globalStyles from "../../../../constants/globalStyles";
@@ -20,72 +19,76 @@ const Tutorial = () => {
   }, []);
 
   return (
-    <SafeAreaView
+    <View
       style={[
         globalStyles.secondaryContainer,
         roleID === 4 && { paddingTop: 0 },
       ]}
     >
-      <View style={styles.info}>
+      <View style={[styles.info]}>
         <View style={styles.infoContainer}>
           <View style={styles.welcomeContainer}>
             <Text style={styles.welcomeText}>EVENTLOG TUTORIAL</Text>
           </View>
-          <Text style={styles.infoText}>
-            To use EVENTLOG, here’s a step-by-step guide to get you started:
-          </Text>
-          <Text style={styles.infoText}>
-            1. Navigating the App {"\n"}
-            {"\u2022"} Open the EVENTLOG app, then Log In. {"\n"}
-            {"\u2022"}The Home page is for the Announcements.{"\n"}
-            {"\u2022"}The QR Code page is for generating QR Code. {"\n"}
-            {"\u2022"}The Records page is for attendance records. {"\n"}
-            {"\u2022"}The Account page is for user's information, contact
-            information of EVENTLOG, and Log Out button.
-          </Text>
-          <Text style={styles.infoText}>
-            2. Attendance Time In/Out
-            {"\n"}
-            {"\u2022"}Navigate to the QR Code tab. {"\n"}
-            {"\u2022"}Browse through the list of available events. {"\n"}
-            {"\u2022"}Click on the event you want to join.{"\n"}
-            {"\u2022"}Generate your own QR Code. {"\n"}
-            {"\u2022"}Show your QR code to the instructors, or student officers
-            in-charge.
-          </Text>
-          <Text style={styles.infoText}>
-            3. Viewing Your Attendance Records{"\n"}
-            {"\u2022"}Navigate to the Records tab. {"\n"}
-            {"\u2022"}Click on the event you want to view. {"\n"}
-            {"\u2022"}Download the attendance record (optional).
-          </Text>
-          <Text style={styles.infoText}>
-            4. Troubleshooting and Support{"\n"}
-            {"\u2022"} If you encounter any issues, please contact the COLLEGE
-            OF INFORMATION TECHNOLOGY DEPARTMENT for troubleshooting and
-            support.
-          </Text>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text style={styles.infoText}>
+              To use EVENTLOG, here’s a step-by-step guide to get you started:
+            </Text>
+            <Text style={styles.infoText}>
+              1. Navigating the App {"\n"}
+              {"\u2022"} Open the EVENTLOG app, then Log In. {"\n"}
+              {"\u2022"}The Home page is for the Announcements.{"\n"}
+              {"\u2022"}The QR Code page is for generating QR Code. {"\n"}
+              {"\u2022"}The Records page is for attendance records. {"\n"}
+              {"\u2022"}The Account page is for user's information, contact
+              information of EVENTLOG, and Log Out button.
+            </Text>
+            <Text style={styles.infoText}>
+              2. Attendance Time In/Out
+              {"\n"}
+              {"\u2022"}Navigate to the QR Code tab. {"\n"}
+              {"\u2022"}Browse through the list of available events. {"\n"}
+              {"\u2022"}Click on the event you want to join.{"\n"}
+              {"\u2022"}Generate your own QR Code. {"\n"}
+              {"\u2022"}Show your QR code to the instructors, or student
+              officers in-charge.
+            </Text>
+            <Text style={styles.infoText}>
+              3. Viewing Your Attendance Records{"\n"}
+              {"\u2022"}Navigate to the Records tab. {"\n"}
+              {"\u2022"}Click on the event you want to view. {"\n"}
+              {"\u2022"}Download the attendance record (optional).
+            </Text>
+            <Text style={styles.infoText}>
+              4. Troubleshooting and Support{"\n"}
+              {"\u2022"} If you encounter any issues, please contact the COLLEGE
+              OF INFORMATION TECHNOLOGY DEPARTMENT for troubleshooting and
+              support.
+            </Text>
 
-          <View style={styles.contactsContainer}>
-            <View style={styles.emailContainer}>
-              <Image source={images.email} style={styles.icon} />
-              <Text style={styles.infoText}>cit_eventlogsupport@gmail.com</Text>
+            <View style={styles.contactsContainer}>
+              <View style={styles.emailContainer}>
+                <Image source={images.email} style={styles.icon} />
+                <Text style={styles.infoText}>
+                  cit_eventlogsupport@gmail.com
+                </Text>
+              </View>
+              <View style={styles.facebookContainer}>
+                <Image source={images.facebook} style={styles.icon} />
+                <Text style={styles.infoText}>CITofficial.UCV</Text>
+              </View>
+              <View style={styles.locationContainer}>
+                <Image source={images.location} style={styles.icon} />
+                <Text style={styles.infoText}>
+                  CIT Office - VHNPB Building 4th Floor
+                </Text>
+              </View>
             </View>
-            <View style={styles.facebookContainer}>
-              <Image source={images.facebook} style={styles.icon} />
-              <Text style={styles.infoText}>CITofficial.UCV</Text>
-            </View>
-            <View style={styles.locationContainer}>
-              <Image source={images.location} style={styles.icon} />
-              <Text style={styles.infoText}>
-                CIT Office - VHNPB Building 4th Floor
-              </Text>
-            </View>
-          </View>
+          </ScrollView>
         </View>
       </View>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -109,9 +112,10 @@ const styles = StyleSheet.create({
     fontSize: theme.fontSizes.large,
   },
   info: {
-    flex: 1,
+    height: "100%",
     backgroundColor: theme.colors.primary,
-    marginBottom: 30,
+    margin: 10,
+    marginBottom: 40,
   },
   infoContainer: {
     flex: 1,
